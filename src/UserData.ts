858694351ac6571768336c578c0b8e711f05ef04
youkan <youkan94@gmail.com>
2019-12-15 00:32:56 +0900

    feat: update UserData.ts

--- src/UserData.ts
+++ src/UserData.ts
@@ -5,4 +5,7 @@ export const UserData:any = {
   // プロジェクトの読み込みたいフォルダをコンマ(,)区切り指定してください。
   // ex: render,material,reference
   "ProjectFolders": "",
+  // 連番画像のうちのはじめのファイル名を入力して下さい。
+  // ex: Image0001.png
+  "ProjectBaseFileName": ""
 };
