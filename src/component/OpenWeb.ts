2ae27c60b4295e23bd65f3b844a3a7999e0e1d78
youkan <youkan94@gmail.com>
2019-12-15 16:16:31 +0900

    feat: cleanup and update openweb.ts

--- src/component/OpenWeb.ts
+++ src/component/OpenWeb.ts
@@ -6,21 +6,29 @@ export default class OpenWeb {
 
   public urls: string[] = [];
 
-  constructor(inputUrl: any = null) {
+  constructor(inputUrl: string | string[] | null = null) {
     if( inputUrl != null ){
-      if( _.getType(inputUrl) === "string" ){
+      if( typeof inputUrl === "string" && _.getType(inputUrl) === "string" ){
         this.setUrl(inputUrl);
       }
-      if( _.getType(inputUrl) === "array" ){
+      if( typeof inputUrl === "object" && _.getType(inputUrl) === "array" ){
         this.setUrls(inputUrl);
       }
     }
   }
 
+  /**
+   * 一つのURLをセットする
+   * @param url 
+   */
   setUrl(url: string): void {
     this.urls.push(url)
   }
 
+  /**
+   * 複数のURLをまとめてセットする
+   * @param urlArray 
+   */
   setUrls(urlArray: string[]): void{
     for( const url of urlArray ){
       this.urls.push(url);
@@ -31,18 +39,23 @@ export default class OpenWeb {
     this.urls.push(URLs.blend);
   }
   
-  getUrl(order: number): string | undefined{
+  getUrl(order: number): string | boolean{
     if( this.urls[order] != null ){
       return this.urls[order];
     }
-    return undefined;
+    return false;
   }
 
+  /**
+   * セットされたURL配列を取得する
+   */
   getUrls():string[] {
     return this.urls;
   }
 
-
+  /**
+   * セットされたすべてのURL配列を削除する
+   */
   removeAllUrl(): void{
     this.urls = [];
   }
@@ -51,17 +64,30 @@ export default class OpenWeb {
 
   }
 
+  /**
+   * リファレンス用のサイトをブラウザで開く
+   * @param searchStr 検索したい文字
+   * @todo 多言語化、UTF8対応、タイプごとの絞り込み、継承して別クラスにしたほうがいい？、Fileでsettingファイルを保存できると楽？（どこだけ開くとか）、サジェスチョン
+   */
   searchReference(searchStr: string){
 
     this.removeAllUrl();
 
-    for( let key of Object.keys(SeachUrls) ){
-      let url: string = `${SeachUrls[key]["url"]}`;
-      const seachType = SeachUrls[key]["seachType"];
+    for( const key of Object.keys(SeachUrls) ){
 
-      if( seachType === "query" ){
-        const baseQuery = SeachUrls[key]["baseQuery"];
-        const seachQuery = SeachUrls[key]["searchQuery"];
+      /**
+       * 最終的に生成されるURL
+       */
+      let url: string = `${SeachUrls[key]["url"]}`;
+
+      /**
+       * クエリ型か、URL型でURLを構成しているか
+       */
+      const searchType: string = SeachUrls[key]["searchType"];
+
+      if( searchType === "query" ){
+        const baseQuery: string = SeachUrls[key]["baseQuery"];
+        const seachQuery: string = SeachUrls[key]["searchQuery"];
 
         url += "?";
 
@@ -72,10 +98,9 @@ export default class OpenWeb {
 
         url = url.replace("&", "^&");
 
-      } else if( seachType === "url" ){
+      } else if( searchType === "url" ){
         url += searchStr;
       }
-
 
       this.setUrl(url);
 
