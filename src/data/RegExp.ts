e859ad350124362be8585c947ad91b1b8f08a297
youkan <youkan94@gmail.com>
2019-12-20 05:10:50 +0900

    feat: 正規表現追加

---
+++ src/data/RegExp.ts
@@ -0,0 +1,17 @@
+export const RegExp = {
+  "url": {
+    val: "^(http|https)://([\w-]+\.)+[\w-]+(/[\w-./?%&=]*)?$",
+  },
+  "num": {
+    val: "^[0-9]*$",
+  },
+  "en": {
+    val: "^[A-Za-z]+$",
+  },
+  "enSmall": {
+    val: "^[a-z]+$",
+  },
+  "enBig": {
+    val: "^[A-Z]+$"
+  },
+}
