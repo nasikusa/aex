d83124f979b44ff99d0ad6478076794aff68436a
youkan <youkan94@gmail.com>
2019-12-20 05:10:33 +0900

    refactor: フォルダ項目追加

--- src/data/Folders.ts
+++ src/data/Folders.ts
@@ -1,11 +1,26 @@
 export const Folders = {
   "material-water": {
     path: "D:/googledrive/material/water_color/",
+    type: "asset",
   },
   "material": {
     path: "D:/googledrive/material/",
+    type: "asset",
   },
   "mateiral-gradation": {
-    path: "D:/googledrive/material/gradation/"
+    path: "D:/googledrive/material/gradation/",
+    type: "asset",
   },
+  "render": {
+    path: "D:/googledrive/render/",
+    type: "render",
+  },
+  "idea": {
+    path: "D:/googledrive/idea/",
+    type: "idea"
+  },
+  "project": {
+    path: "D:/googledrive/works/",
+    type: "project",
+  }
 };
