e814e5ed0b5f298bf302d3bfc8f4219e05cbf60d
youkan <youkan94@gmail.com>
2019-12-20 04:03:59 +0900

    feat: EFクラスの関数の更新

--- src/component/effect/EF.ts
+++ src/component/effect/EF.ts
@@ -5,10 +5,10 @@ export default class EF {
     return Effects[name];
   }
 
-  static getDataHasName(englishName: string , type: string){
+  static getDataHasName(name: string , type: string){
     for( let key of Object.keys(Effects) ){
       if( !Effects[key][type] ) continue;
-      if( Effects[key][type] === englishName ){
+      if( Effects[key][type] === name ){
         return Effects[key];
       }
     }
@@ -18,35 +18,45 @@ export default class EF {
   static has(name: string){
     return EF.getDataByName(name);
   }
-  static by(name: string){
+  static by(name: string): string{
     return EF.getDataByName(name).match;
   }
 
   static hasEn(englishName: string){
     return EF.getDataHasName(englishName , "en");
   }
-  static hasJa(englishName: string){
-    return EF.getDataHasName(englishName , "ja");
+  static hasJa(janeneseName: string){
+    return EF.getDataHasName(janeneseName , "ja");
   }
-  static hasQ(englishName: string){
-    return EF.getDataHasName(englishName , "q");
+  static hasQ(quickName: string){
+    return EF.getDataHasName(quickName , "q");
   }
-  static hasM(englishName: string){
-    return EF.getDataHasName(englishName , "match");
+  static hasM(matchName: string){
+    return EF.getDataHasName(matchName , "match");
   }
 
-  static byEn(englishName: string){
-    return EF.getDataHasName(englishName , "en").match;
+  static byEn(englishName: string): string | undefined{
+    const URLObject = EF.hasEn(englishName);
+    if( typeof URLObject === "object"){
+      return URLObject.match;
   }
   static byJa(englishName: string){
     return EF.getDataHasName(englishName , "ja").match;
   }
-  static byQ(englishName: string){
-    return EF.getDataHasName(englishName , "q").match;
+  static byJa(japaneseName: string){
+    const URLObject = EF.hasJa(japaneseName);
+    if( typeof URLObject === "object"){
+      return URLObject.match;
   }
   static byM(englishName: string){
     return EF.getDataHasName(englishName , "match").match;
   }
 
+  static byQ(quickName: string){
+    const URLObject = EF.hasQ(quickName);
+    if( typeof URLObject === "object"){
+      return URLObject.match;
+    }
+  }
 
 }
