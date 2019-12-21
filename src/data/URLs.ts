2adc270bb415170c9ee6636fdec612cab0ef6cb8
youkan <youkan94@gmail.com>
2019-12-21 16:07:27 +0900

    style: prettier

--- src/data/URLs.ts
+++ src/data/URLs.ts
@@ -1,80 +1,83 @@
 interface URLsContent {
-  readonly url: string;
+  readonly url: string
 }
 
 interface SeachUrlsContent extends URLsContent {
-  readonly type: string;
-  readonly searchType: string;
-  readonly baseQuery: string;
-  readonly searchQuery: string;
-  readonly expectedLang: string;
-};
+  readonly type: 'image' | '3d' | 'video'
+  readonly searchType: 'url' | 'query'
+  readonly baseQuery: string
+  readonly searchQuery: string
+  readonly expectedLang: string
+}
 
-
-export const URLs: {[key: string]: URLsContent} = {
-  "blend-monitoring": {
-    url: "http://nasikusa.net/blend-monitoring"
+export const URLs: {
+  [key: string]: URLsContent
+} = {
+  'blend-monitoring': {
+    url: 'http://nasikusa.net/blend-monitoring'
   },
-  "colorSelector": {
-    url: "http://nasikusa.net/practice/p39.html"
-  },
-};
+  colorSelector: {
+    url: 'http://nasikusa.net/practice/p39.html'
+  }
+}
 
 /**
  * リファレンス検索用のURLおよび、その他の情報を貯蓄しておく配列
  * @todo sketchfabのダウンロード可能などのその他のクエリへの対応
  */
-export const SeachUrls: {[key: string]: SeachUrlsContent} = {
-  "googleImage": {
+export const SeachUrls: {
+  [key: string]: SeachUrlsContent
+} = {
+  googleImage: {
     type: 'image',
-    searchType: "query",
-    url: "https://www.google.com/search",
-    baseQuery: "tbm=isch",
-    searchQuery: "q",
-    expectedLang: "all",
+    searchType: 'query',
+    url: 'https://www.google.com/search',
+    baseQuery: 'tbm=isch',
+    searchQuery: 'q',
+    expectedLang: 'all'
   },
-  "youtube": {
-    type: "video",
-    searchType: "query",
-    url: "https://www.youtube.com/results",
-    baseQuery: "",
-    searchQuery: "search_query",
-    expectedLang: "all",
+  youtube: {
+    type: 'video',
+    searchType: 'query',
+    url: 'https://www.youtube.com/results',
+    baseQuery: '',
+    searchQuery: 'search_query',
+    expectedLang: 'all'
   },
-  "vimeo": {
-    type: "video",
-    searchType: "query",
-    url: "https://vimeo.com/search",
-    baseQuery: "",
-    searchQuery: "q",
-    expectedLang: "english",
+  vimeo: {
+    type: 'video',
+    searchType: 'query',
+    url: 'https://vimeo.com/search',
+    baseQuery: '',
+    searchQuery: 'q',
+    expectedLang: 'english'
   },
-  "sketchfab": {
-    type: "3d",
-    searchType: "query",
-    url: "https://sketchfab.com/search",
-    baseQuery: "",
-    searchQuery: "q",
-    expectedLang: "english",
+  sketchfab: {
+    type: '3d',
+    searchType: 'query',
+    url: 'https://sketchfab.com/search',
+    baseQuery: '',
+    searchQuery: 'q',
+    expectedLang: 'english'
   },
-  "unsplash": {
-    type: "image",
-    searchType: "url",
-    url: "https://unsplash.com/s/photos/",
-    baseQuery: "",
-    searchQuery: "",
-    expectedLang: "english",
+  unsplash: {
+    type: 'image',
+    searchType: 'url',
+    url: 'https://unsplash.com/s/photos/',
+    baseQuery: '',
+    searchQuery: '',
+    expectedLang: 'english'
   },
-  "PAKUTASO": {
-    type: "image",
-    searchType: "query",
-    url: "https://www.pakutaso.com/search.html",
-    baseQuery: "offset=0&limit=20",
-    searchQuery: "search",
-    expectedLang: "japanese",
+  PAKUTASO: {
+    type: 'image',
+    searchType: 'query',
+    url: 'https://www.pakutaso.com/search.html',
+    baseQuery: 'offset=0&limit=20',
+    searchQuery: 'search',
+    expectedLang: 'japanese'
   }
-};
+}
 
-export const MusicURLs: {[key: string]: URLsContent} = {
-
-};
+export const MusicURLs: {
+  [key: string]: URLsContent
+} = {}
