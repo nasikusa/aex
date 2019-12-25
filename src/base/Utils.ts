e9ee2776a47940e38bb47409679aa56c529bb3f7
youkan <youkan94@gmail.com>
2019-12-26 00:05:24 +0900

    style: prettier

--- src/base/Utils.ts
+++ src/base/Utils.ts
@@ -5,65 +5,122 @@
  * @class Utils
  */
 export default class Utils {
-
   /**
    * 型情報を取得する
    * @param obj
    */
-  static getType(obj: any){
+  static getType(obj: any) {
     const toString = Object.prototype.toString;
-    return toString.call(obj).slice(8, -1).toLowerCase();
+    return toString
+      .call(obj)
+      .slice(8, -1)
+      .toLowerCase();
   }
 
-  static joinLine(inputArray: any[]){
+  static joinLine(inputArray: any[]) {}
 
-  }
-
-  static getRandomFromArray(array: any): any{
-    if( Array.isArray(array) && Utils.getType(array) === 'array' ){
+  static getRandomFromArray(array: any): any {
+    if (Array.isArray(array) && Utils.getType(array) === 'array') {
       return array[Math.floor(Math.random() * array.length)];
     }
     return false;
   }
 
-  static randomMinMax(min: number , max: number){
+  static randomMinMax(min: number, max: number) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
 
-  static trim(inputString: string): string{
-    return inputString.replace(/^\s+|\s+$/g, "");
+  /**
+   *空白文字をトリムする
+   *
+   * @static
+   * @param {string} inputString
+   * @returns {string}
+   * @memberof Utils
+   */
+  static trim(inputString: string): string {
+    return inputString.replace(/^\s+|\s+$/g, '');
   }
 
-  static getMinFromArray(inputArray: number[]): number{
-    return Math.min.apply(Math, inputArray)
+  /**
+   *配列から最小の値を得る
+   *
+   * @static
+   * @param {number[]} inputArray
+   * @returns {number}
+   * @memberof Utils
+   */
+  static getMinFromArray(inputArray: number[]): number {
+    return Math.min.apply(Math, inputArray);
   }
 
-  static getMaxFromArray(inputArray: number[]): number{
+  /**
+   *配列から最大の値を得る
+   *
+   * @static
+   * @param {number[]} inputArray
+   * @returns {number}
+   * @memberof Utils
+   */
+  static getMaxFromArray(inputArray: number[]): number {
     return Math.max.apply(Math, inputArray);
   }
 
-  static array2Empty(array: any): any{
-    if( Array.isArray(array) && Utils.getType(array) === 'array' ){
-      array.length = 0;
-      return array;
-    }
-    return false;
+  /**
+   *配列を空にする
+   *
+   * @static
+   * @param {any[]} array
+   * @returns {any[]}
+   * @memberof Utils
+   */
+  static array2Empty(array: any[]): any[] {
+    array.length = 0;
+    return array;
   }
 
   // https://ics.media/entry/11292/
-  static getAddRandom(): number{
+  /**
+   *加算ランダム値を得る
+   *
+   * @static
+   * @returns {number}
+   * @memberof Utils
+   */
+  static getAddRandom(): number {
     return (Math.random() + Math.random()) / 2;
   }
 
+  /**
+   *乗算のランダム値を得る
+   *
+   * @static
+   * @returns {number}
+   * @memberof Utils
+   */
   static getMultiplyRandom(): number {
-    return Math.random() * Math.random()
+    return Math.random() * Math.random();
   }
 
+  /**
+   *
+   *
+   * @static
+   * @returns {number}
+   * @memberof Utils
+   */
   static getSqrtRandom(): number {
     return Math.sqrt(Math.random());
   }
 
-  static getNormalRandom(): number{
+  /**
+   *正規分布のランダム値を得る
+   *
+   * @static
+   * @returns {number}
+   * @memberof Utils
+   */
+  static getNormalRandom(): number {
     // 0.0未満、1.0以上になるケースがあるため
     // その時は再計算を行う
     var value;
@@ -80,16 +137,13 @@ export default class Utils {
     // 正規乱数
     var r1 = Math.random();
     var r2 = Math.random();
-    var value =
-      Math.sqrt(-2.0 * Math.log(r1)) *
-      Math.sin(2.0 * Math.PI * r2);
+    var value = Math.sqrt(-2.0 * Math.log(r1)) * Math.sin(2.0 * Math.PI * r2);
     // 値を0以上1未満になるよう正規化する
     value = (value + 3) / 6;
     return value;
   }
 
-  static getArrayDividedByLine(inputText: string): string[]{
+  static getArrayDividedByLine(inputText: string): string[] {
     return inputText.split(/\n/);
   }
-
 }
