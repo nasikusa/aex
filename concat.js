7c9fe2869d7e25c48f0b7f0cca4b0c1048e66419
nasikusa <youkan94@gmail.com>
2020-01-01 21:30:45 +0900

    chore: 不要ファイル削除

--- concat.js
+++
@@ -1,9 +0,0 @@
-var concat = require('concat-files');
-
-concat([
-  './src/UI/__GlobalExtendThis__.js',
-  './dist/main.js',
-], './test3.js', function(err) {
-  if (err) throw err
-  console.log('done');
-});
