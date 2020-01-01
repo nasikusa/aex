3def215690e148d363d5df64fba612c7b77e4e9f
nasikusa <youkan94@gmail.com>
2020-01-01 14:28:44 +0900

    refactor: prettier

--- src/module/command/OpenWebReference.ts
+++ src/module/command/OpenWebReference.ts
@@ -1,15 +1,14 @@
-import {URLs, SeachUrls} from '../../data/URLs';
-import _ from '../../base/_';
-import CallCommand from './CallCommand';
+import { SeachUrls } from '../../data/URLs';
+// import _ from '../../base/_';
+// import CallCommand from './CallCommand';
 import OpenWeb from './OpenWeb';
 
 export default class OpenWebReference extends OpenWeb {
-
   /**
    * 初期化時にURLが引数にあれば、それをそのままセットする。なければスルー。
    * @param inputUrl
    */
-  constructor(inputUrl: string | string[] | null = null) {
+  constructor(inputUrl?: string | string[]) {
     super(inputUrl);
   }
 
@@ -18,50 +17,43 @@ export default class OpenWebReference extends OpenWeb {
    * @param searchStr 検索したい文字
    * @todo 多言語化、UTF8対応、タイプごとの絞り込み、継承して別クラスにしたほうがいい？、Fileでsettingファイルを保存できると楽？（どこだけ開くとか）、サジェスチョン、URLエンコードするとgoogleで検索できなくなる
    */
-  searchReference(searchStr: string): boolean{
-
+  searchReference(searchStr: string): boolean {
     this.removeAllUrl();
 
-    for( const key of Object.keys(SeachUrls) ){
-
+    for (const key of Object.keys(SeachUrls)) {
       /**
        * 最終的に生成されるURL
        */
-      let url: string = `${SeachUrls[key]["url"]}`;
+      let url = `${SeachUrls[key]['url']}`;
 
       /**
        * クエリ型か、URL型でURLを構成しているか
        */
-      const searchType: string = SeachUrls[key]["searchType"];
+      const searchType: string = SeachUrls[key]['searchType'];
 
-      if( searchType === "query" ){
-        const baseQuery: string = SeachUrls[key]["baseQuery"];
-        const seachQuery: string = SeachUrls[key]["searchQuery"];
+      if (searchType === 'query') {
+        const baseQuery: string = SeachUrls[key]['baseQuery'];
+        const seachQuery: string = SeachUrls[key]['searchQuery'];
 
-        url += "?";
+        url += '?';
 
-        if( baseQuery ){
+        if (baseQuery) {
           url += `${baseQuery}&`;
         }
         url += `${seachQuery}=${searchStr}`;
 
-        url = url.replace("&", "^&");
-
-      } else if( searchType === "url" ){
+        url = url.replace('&', '^&');
+      } else if (searchType === 'url') {
         url += searchStr;
       }
 
       url = encodeURI(url);
 
       this.setUrl(url);
-
     }
 
     const isOpenSite = this.open();
 
     return isOpenSite;
-
   }
-
-
 }
