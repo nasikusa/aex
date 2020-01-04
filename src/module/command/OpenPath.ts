ad1b44b02caafb66bd18d7d7d2a1afe7431712a4
nasikusa <youkan94@gmail.com>
2020-01-04 20:48:53 +0900

    style: jsdocなど

--- src/module/command/OpenPath.ts
+++ src/module/command/OpenPath.ts
@@ -3,6 +3,13 @@ import { Folders } from '../../data/Folders';
 import { AppPath } from '../../data/Paths';
 import _ from '../../base/_';
 
+/**
+ *フォルダやアプリなどのパス指定でのオープンを担当するクラス
+ *
+ * @export
+ * @class OpenPath
+ * @todo appを開く関数の修正 , urlの正規表現でのチェック
+ */
 export default class OpenPath {
   /**
    *開くパスの配列
@@ -12,6 +19,11 @@ export default class OpenPath {
    */
   public paths: string[] = [];
 
+  /**
+   *パスが引数に入っていた場合は、初期化する
+   * @param {(string | string[])} [path] パスもしくはパス配列
+   * @memberof OpenPath
+   */
   constructor(path?: string | string[]) {
     this.init(path);
   }
@@ -19,10 +31,10 @@ export default class OpenPath {
   /**
    *初期化関数
    *
-   * @param {(string | string[])}
+   * @param {(string | string[])} パスもしくはパス配列
    * @memberof OpenPath
    */
-  init(path?: string | string[]) {
+  init(path?: string | string[]): void {
     if (path != null) {
       if (typeof path === 'string') {
         this.setPath(path);
@@ -35,8 +47,8 @@ export default class OpenPath {
   /**
    *開くパスをセットする
    *
-   * @param {string} pathName
-   * @returns {OpenPath}
+   * @param {string} pathName パス名
+   * @returns {OpenPath} thisを返却する
    * @memberof OpenPath
    */
   setPath(pathName: string): OpenPath {
@@ -47,9 +59,10 @@ export default class OpenPath {
   /**
    *開くパスを複数セットする
    *
-   * @param {string[]} pathNameArray
-   * @returns {OpenPath}
+   * @param {string[]} pathNameArray パス配列
+   * @returns {OpenPath} thisを返却する
    * @memberof OpenPath
+   * @deprecated setPath関数に対して、可長変引数で設定できた方が便利だと思う。配列からの入力も、 ...array みたいな感じで対応できるし。
    */
   setPaths(pathNameArray: string[]): OpenPath {
     for (const pathName of pathNameArray) {
@@ -61,8 +74,8 @@ export default class OpenPath {
   /**
    *フォルダパスをフォルダオブジェクトの名前キーから取得して、設定する
    *
-   * @param {string} folderName
-   * @returns {(boolean | string)} 失敗の際はfalse , 成功の際はパスを返す
+   * @param {string} folderName フォルダオブジェクト内のフォルダ名を指定
+   * @returns {boolean} 失敗の際はfalse , 成功の際はtrueを返す
    * @memberof OpenFolder
    */
   setFolderByName(folderName: string): boolean {
@@ -74,35 +87,37 @@ export default class OpenPath {
   }
 
   // TODO: なぜかAE側が固まって動かなくなってしまう（アプリを立ち上げると固まる）
-  setAppByName(appName: string): boolean {
-    if (AppPath[appName] != null) {
-      this.paths.push(` "${AppPath[appName].path}"`);
-      return true;
-    }
-    return false;
-  }
+  // setAppByName(appName: string): boolean {
+  //   if (AppPath[appName] != null) {
+  //     this.paths.push(` "${AppPath[appName].path}"`);
+  //     return true;
+  //   }
+  //   return false;
+  // }
 
   /**
    *すべてのセットされたパスを削除する
    *
    * @memberof OpenPath
    */
-  removeAllPath() {
+  removeAllPath(): void {
     this.paths = [];
   }
 
   /**
    * パスを開くコマンドを実行する
    *
-   * @returns {boolean}
+   * @returns {boolean} 成否
    * @memberof OpenFolder
+   * @todo mac対応
    */
   open(): boolean {
-    let commandArray: string[] = [];
+    const commandArray: string[] = [];
 
-    for (let path of this.paths) {
-      commandArray.push(`start "" "${path}"`);
-    }
+    if (_.isWindows()) {
+      for (const path of this.paths) {
+        commandArray.push(`start "" "${path}"`);
+      }
     } else if (_.isMac()) {
       for (const path of this.paths) {
         // alert(`open "${path}"`);
